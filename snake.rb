267db4d95b22b5ffabf7067004f8f5eadb8036ec
Adam Hystrix <hojer7@o2.pl>
2016-06-13 22:26:08 +0200

    snake can die now

--- snake.rb
+++ snake.rb
@@ -2,11 +2,11 @@ require 'gosu'
 require_relative 'point'
 class Snake
   BLACK = Gosu::Color.argb(0xff_000000)
-  attr_accessor :snake_position, :direction, :last_position
+  attr_reader :snake_position, :direction, :last_position, :alive
   def initialize(window)
     @window = window
     @snake_position = Array.new(3) {|index| Point.new(160, 40*(index+4))}
-    @last_position
+    @alive = true
     @direction = 1
   end
 
@@ -19,27 +19,41 @@ class Snake
   def movement
     case @direction
     when 0
-      @snake_position.unshift(Point.new(@snake_position[0].x-40, @snake_position[0].y))
-    when 1
-      @snake_position.unshift(Point.new(@snake_position[0].x, @snake_position[0].y-40))
-    when 2
-      @snake_position.unshift(Point.new(@snake_position[0].x+40, @snake_position[0].y))
-    when 3
-      @snake_position.unshift(Point.new(@snake_position[0].x, @snake_position[0].y+40))
+      next_pos = calculate_next_pos(-40, 0)
+      when 1
+        next_pos = calculate_next_pos(0, -40)
+      when 2
+        next_pos = calculate_next_pos(40, 0)
+      when 3
+        next_pos = calculate_next_pos(0, 40)
     end
+    lethal_movement(next_pos)
+    return unless @alive
+    @snake_position.unshift(next_pos)
     @last_position = @snake_position.pop
   end
 
+  def lethal_movement(next_pos)
+    @snake_position.drop(1).each do |pos|
+      if pos == next_pos || next_pos.x > 360 || next_pos.x < 0 || next_pos.y < 0 || next_pos.y > 360
+        @alive = false
+      end
+    end
+  end
+
+  def calculate_next_pos(x = 0, y = 0)
+    Point.new(@snake_position[0].x + x, @snake_position[0].y + y)
+  end
   def move_up
-    @direction = 1 unless @direction == 3;
+    @direction = 1 unless @direction == 3 || calculate_next_pos(0, -40) == @snake_position[1]
   end
   def move_down
-    @direction = 3 unless @direction == 1;
+    @direction = 3 unless @direction == 1 || calculate_next_pos(0, 40) == @snake_position[1]
   end
   def move_left
-    @direction = 0 unless @direction == 2;
+    @direction = 0 unless @direction == 2 || calculate_next_pos(-40, 0) == @snake_position[1]
   end
   def move_right
-    @direction = 2 unless @direction == 0;
+    @direction = 2 unless @direction == 0 || calculate_next_pos(40, 0) == @snake_position[1]
   end
 end
